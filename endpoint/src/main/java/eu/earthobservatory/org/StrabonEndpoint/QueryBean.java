5c8a8757b5c09b878aff03efcac7489dee9b2a28
Babis Nikolaou <charnik@di.uoa.gr>
2012-04-04 21:25:40 +0300

    added UpdateBean for updates separating them from execution of SELECT queries in QueryBean, clean-up of QueryBean

--- endpoint/src/main/java/eu/earthobservatory/org/StrabonEndpoint/QueryBean.java
+++ endpoint/src/main/java/eu/earthobservatory/org/StrabonEndpoint/QueryBean.java
@@ -30,7 +30,8 @@ public class QueryBean extends HttpServlet {
 	
 	private static final long serialVersionUID = -378175118289907707L;
 	
-	public QueryBean() {}
+	private ServletContext context; 
+	private StrabonBeanWrapper strabonWrapper;
 	
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 	throws ServletException, IOException
@@ -86,7 +87,12 @@ public class QueryBean extends HttpServlet {
 		String reqFuncionality = (request.getParameter("submit") == null) ? "" : request.getParameter("submit");
 		
 		// check whether Update submit button was fired
-		boolean isUpdate = (reqFuncionality.equals("Update") ? true:false);
+		if (reqFuncionality.equals("Update")) { // only for executions from web browsers
+			response.setStatus(HttpServletResponse.SC_TEMPORARY_REDIRECT);
+			response.sendRedirect("Update?SPARQLQuery=" + hive.getSPARQLQuery());
+			
+			return;
+		}
 		
 		if ((reqFormat == "") && (reqAccept == "")) {
 			hive.setFormat("HTML");
@@ -115,31 +121,6 @@ public class QueryBean extends HttpServlet {
 		}
 		//System.out.println("\n\n\n\n\format='"+this.format+"'\n\n\n\n\n");
 		
-		ServletContext context = getServletContext();
-		WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(context);
-		StrabonBeanWrapper strabonWrapper = (StrabonBeanWrapper)applicationContext.getBean("strabonBean");
-		
-		/* Execution of UPDATE query */  
-		if (isUpdate) {
-			String answer = "";
-			try {
-				strabonWrapper.getStrabon().update(hive.getSPARQLQuery(), strabonWrapper.getStrabon().getSailRepoConnection());
-				response.setStatus(HttpServletResponse.SC_OK);
-				answer = "true";
-				
-			} catch(MalformedQueryException e) {
-				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-				answer = "<exception>\n"+e.getMessage()+"\n\t</exception>";
-			}
-			
-			// write response to client
-			response.getWriter().append(getUPDATEHeaderResponse());
-			response.getWriter().append(answer);
-			response.getWriter().append(getUPDATEFooterResponse());
-			return;
-		}
-		/* Execution of UPDATE query */
-
 		PrintWriter out = response.getWriter();
 		
 		if ((hive.getFormat().equalsIgnoreCase("KML"))) {
@@ -177,9 +158,9 @@ public class QueryBean extends HttpServlet {
  
 	            //System.out.println("Done");
  
-    	     }catch(IOException e){
+    	     } catch(IOException e) {
     		        e.printStackTrace();
-    	           }
+    	     }
     	     
  			 response.setContentType("application/vnd.google-earth.kml+xml; charset=UTF-8");
  			 response.setDateHeader("Expires", 0);
@@ -258,26 +239,14 @@ public class QueryBean extends HttpServlet {
 		out.flush();
 	}
 
-	/**
-	 * Used as the template answer for UPDATE queries.
-	 * @return
-	 */
-	private String getUPDATEHeaderResponse() {
-		return "<?xml version='1.0' encoding='UTF-8'?>\n" +
-			   "<response>\n" +
-			   "\t";
-	}
-	
-	/**
-	 * Used as the template answer for UPDATE queries.
-	 * @return
-	 */
-	private String getUPDATEFooterResponse() {
-		return "\n</response>\n";
-	}
-
 	public void init(ServletConfig servletConfig) throws ServletException {
 		super.init(servletConfig);
+		
+		// get StrabonWrapper
+		context = getServletContext();
+		WebApplicationContext applicationContext = WebApplicationContextUtils.getWebApplicationContext(context);
+		
+		strabonWrapper = (StrabonBeanWrapper)applicationContext.getBean("strabonBean");
 	}
 
 	public String evaluateQuery(StrabonBeanWrapper strabonWrapper, String resultFormat, String reqFunctionality, String SPARQLQuery, StringBuilder errorMessage) {		
