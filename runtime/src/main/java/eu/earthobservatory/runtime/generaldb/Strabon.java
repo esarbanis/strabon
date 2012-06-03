48e7dbaf1bed512124d3a3391d6135c71580c083
Babis Nikolaou <charnik@di.uoa.gr>
2012-06-03 15:20:44 +0300

    organize imports

--- runtime/src/main/java/eu/earthobservatory/runtime/generaldb/Strabon.java
+++ runtime/src/main/java/eu/earthobservatory/runtime/generaldb/Strabon.java
@@ -4,7 +4,6 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
