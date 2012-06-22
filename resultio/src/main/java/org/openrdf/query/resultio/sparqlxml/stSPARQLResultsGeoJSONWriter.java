0f25d4de8542c96a095eaab29ad3a9581e95340d
Babis Nikolaou <charnik@di.uoa.gr>
2012-06-22 20:30:04 +0300

    added dummy class for GeoJSON results encoding. Fill it and fix Bug #05

---
+++ resultio/src/main/java/org/openrdf/query/resultio/sparqlxml/stSPARQLResultsGeoJSONWriter.java
@@ -0,0 +1,32 @@
+package org.openrdf.query.resultio.sparqlxml;
+
+import java.util.List;
+
+import org.openrdf.query.BindingSet;
+import org.openrdf.query.TupleQueryResultHandlerException;
+import org.openrdf.query.resultio.TupleQueryResultFormat;
+import org.openrdf.query.resultio.TupleQueryResultWriter;
+
+public class stSPARQLResultsGeoJSONWriter implements TupleQueryResultWriter {
+
+	@Override
+	public void startQueryResult(List<String> bindingNames)
+			throws TupleQueryResultHandlerException {
+		
+	}
+
+	@Override
+	public void endQueryResult() throws TupleQueryResultHandlerException {
+	}
+
+	@Override
+	public void handleSolution(BindingSet bindingSet)
+			throws TupleQueryResultHandlerException {
+	}
+
+	@Override
+	public TupleQueryResultFormat getTupleQueryResultFormat() {
+		return TupleQueryResultFormat.JSON;
+	}
+
+}
