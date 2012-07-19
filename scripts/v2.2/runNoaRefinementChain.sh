1dc0bc7e18c35d412d232ab1aaaf029f8224ca45
Babis Nikolaou <charnik@di.uoa.gr>
2012-07-19 20:55:17 +0300

    fixed hardcoded values for satellite and sensor. Now these values are taken from the hotspot filename to be processed.

--- scripts/v2.2/runNoaRefinementChain.sh
+++ scripts/v2.2/runNoaRefinementChain.sh
@@ -3,28 +3,11 @@ LOC="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ENDPOINT="http://localhost:8080/endpoint"
 DB="endpoint"
-#GRIDURL="http://kk.di.uoa.gr/grid_4.nt"
-GRIDURL="http://jose.di.uoa.gr/rdf/coastline/grid_4.nt"
-#INIT="http://jose.di.uoa.gr/rdf/Kallikratis-Coastline.ntriples"
-INIT="../Kalli_coast.sql"
-
-#CHECKDIR="/home/konstantina/allhot/"
-#CHECKDIR="${HOME}/teleios/nkua/Hotspots/"
-
-POSTGISTEMPLATE="postgistemplate"
-#POSTGISTEMPLATE="template_postgis"
-
-#dataDir="http://localhost/noa-teleios/out_triples/"
-#dataDir="http://kk.di.uoa.gr/out_triples/"
-#dataDir="http://godel.di.uoa.gr/allhot/"
-dataDir="http://jose.di.uoa.gr/rdf/hotspots/20"
 
 #HOTSPOTS_URL="http://jose.di.uoa.gr/rdf/hotspots"
 HOTSPOTS_URL="http://jose.di.uoa.gr/rdf/hotspots/msg1"
 
 logFile="chain.log"
-#countWTime="/usr/bin/time -p   %e"
-#echo > ${logFile}
 
 function timer()
 {
@@ -64,7 +47,7 @@ function chooseTomcat()
 	elif test -s /etc/lsb-release ; then # Ubuntu
 			tomcat=$(ls -1 /etc/init.d/| grep tomcat | head -1)
 	elif test -s /etc/debian_version ; then
-			tomcat="tomcat"
+		tomcat="tomcat"
 	fi
 
 	# check for service availability
@@ -110,39 +93,54 @@ echo "starting tomcat"
 if test -z "${tomcat}"; then
 	# work-around for babis (standalone tomcat, with start_tomcat.sh and stop_tomcat.sh scripts)
 	start_tomcat.sh
+	sleep 2
 else
 	sudo service ${tomcat} start
 fi
 
 echo "initializing database"
-echo "S M D IF R TP" >stderr.txt
+echo "Timestamp	Store	Municipalities	DeleteInSea	InvalidForFires	RefineInCoast	TimePersistence" > stderr.txt
 
 
 #./scripts/endpoint query ${ENDPOINT} "SELECT (COUNT(*) AS ?C) WHERE {?s ?p ?o}"
 #sudo -u postgres psql -d endpoint -c 'CREATE INDEX datetime_values_idx_value ON datetime_values USING btree(value)';
 #sudo -u postgres psql -d endpoint -c 'VACUUM ANALYZE;';
 
-#echo "Continue?"
-#read a
-
 #for y in 2007 2008 2010 2011 ;do
-for y in 2012 ;do
+for y in 2012; do
 	# get hotpost URLS
 	for hot in $(curl -s ${HOTSPOTS_URL}/${y}/ | grep -o '>HMSG.*\.nt' | colrm 1 1); do
 		file="${HOTSPOTS_URL}/${y}/${hot}"
 
 		time_status=$(echo ${hot} | egrep -o '[[:digit:]]{6}_[[:digit:]]{4}')
+		
+		# get sensor
+		SENSOR=$(echo ${hot} | grep -o 'MSG.')
 
-		# get time information for acquisition
+		# get satellite
+		if test "${SENSOR}" = "MSG2"; then
+			SAT="METEOSAT9"
+		else
+			SAT="METEOSAT8"
+			SENSOR="MSG1_RSS"
+		fi
+
+		# get time information for acquisition and construct timestamp
 		year="20$(expr substr ${time_status} 1 2)"
 		month=$(expr substr ${time_status} 3 2)
 		day=$(expr substr ${time_status} 5 2)
 		time2=$(expr substr ${time_status} 8 2)
 		time2="${time2}:$(expr substr ${time_status} 10 2)"
 
+		# construct timestamp
+		TIMESTAMP="${year}-${month}-${day}T${time2}:00"
+
 		# store file
 		echo -n "storing " $file; echo; echo; 
 		# ${countTime} ./strabon -db endpoint store $file
+
+		# print timestamp
+		echo -n "${TIMESTAMP} " >> stderr.txt
 
 		tmr1=$(timer)
 		../endpoint store ${ENDPOINT} N-Triples -u ${file}
@@ -162,19 +160,19 @@ for y in 2012 ;do
 
 		query=`echo "${insertMunicipalities}" | sed "s/TIMESTAMP/${year}-${month}-${day}T${time2}:00/g" | \
 		sed "s/PROCESSING_CHAIN/DynamicThresholds/g" | \
-		sed "s/SENSOR/MSG2/g"`
+		sed "s/SENSOR/${SENSOR}/g"`
 
 		../endpoint update ${ENDPOINT} "${query}"
 		
 		tmr2=$(timer)
-printf '%s ' $((tmr2-tmr1)) >>stderr.txt
+		printf '%s ' $((tmr2-tmr1)) >>stderr.txt
 		echo;echo;echo;echo "File ${file} inserted Municipalities!"
 		
 		# deleteSeaHotspots
 		echo -n "Going to deleteSeaHotspots ${year}-${month}-${day}T${time2}:00 " ;echo; echo; echo;
 		query=`echo "${deleteSeaHotspots}" | sed "s/TIMESTAMP/${year}-${month}-${day}T${time2}:00/g" | \
 		sed "s/PROCESSING_CHAIN/DynamicThresholds/g" | \
-		sed "s/SENSOR/MSG2/g"`
+		sed "s/SENSOR/${SENSOR}/g"`
 		# ${countTime} ./strabon -db endpoint update "${query}"
 
 		tmr1=$(timer)
@@ -190,8 +188,8 @@ printf '%s ' $((tmr2-tmr1)) >>stderr.txt
 		echo -n "invalidForFires ${year}-${month}-${day}T${time2}:00 "  ; echo; echo ; echo;
 		query=`echo "${invalidForFires}" | sed "s/TIMESTAMP/${year}-${month}-${day}T${time2}:00/g" | \
 		sed "s/PROCESSING_CHAIN/DynamicThresholds/g" | \
-		sed "s/SENSOR/MSG2/g" |\
-		sed "s/SAT/METEOSAT9/g"`
+		sed "s/SENSOR/${SENSOR}/g" |\
+		sed "s/SAT/${SAT}/g"`
 		# ${countTime} ./strabon -db endpoint update "${query}"
 		tmr1=$(timer)
 		../endpoint update ${ENDPOINT} "${query}"
@@ -203,8 +201,8 @@ printf '%s ' $((tmr2-tmr1)) >>stderr.txt
 		echo -n "refinePartialSeaHotspots ${year}-${month}-${day}T${time2}:00 "  ; echo; echo ; echo;
 		query=`echo "${refinePartialSeaHotspots}" | sed "s/TIMESTAMP/${year}-${month}-${day}T${time2}:00/g" | \
 		sed "s/PROCESSING_CHAIN/DynamicThresholds/g" | \
-		sed "s/SENSOR/MSG2/g" |\
-		sed "s/SAT/METEOSAT9/g"`
+		sed "s/SENSOR/${SENSOR}/g" |\
+		sed "s/SAT/${SAT}/g"`
 		# ${countTime} ./strabon -db endpoint update "${query}"
 		tmr1=$(timer)
 		../endpoint update ${ENDPOINT} "${query}"
@@ -220,10 +218,10 @@ printf '%s ' $((tmr2-tmr1)) >>stderr.txt
 		min_acquisition_time=`date --date="${year}-${month}-${day} ${time2}:00 EEST -30 minutes" +%Y-%m-%dT%H:%m:00`
 		query=`echo "${refineTimePersistence}" | sed "s/TIMESTAMP/${year}-${month}-${day}T${time2}:00/g" | \
 		sed "s/PROCESSING_CHAIN/DynamicThresholds/g" | \
-		sed "s/SENSOR/MSG2/g" | \
+		sed "s/SENSOR/${SENSOR}/g" | \
 		sed "s/ACQUISITIONS_IN_HALF_AN_HOUR/3.0/g" | \
 		sed "s/MIN_ACQUISITION_TIME/${min_acquisition_time}/g" |\
-		sed "s/SAT/METEOSAT9/g"`
+		sed "s/SAT/${SAT}/g"`
 
 		#sudo -u postgres psql -d ${DB} -c 'VACUUM ANALYZE;';
 
@@ -242,7 +240,7 @@ printf '%s ' $((tmr2-tmr1)) >>stderr.txt
 		max_acquisition_time=`date --date="${year}-${month}-${day} 23:59 EEST" +%Y-%m-%dT%H:%m:00`
 		query=`echo "${discover}" | \
 			sed "s/PROCESSING_CHAIN/DynamicThresholds/g" | \
-			sed "s/SENSOR/MSG2/g" | \
+			sed "s/SENSOR/${SENSOR}/g" | \
 			sed "s/MIN_ACQUISITION_TIME/${min_acquisition_time}/g" |\
 			sed "s/MAX_ACQUISITION_TIME/${max_acquisition_time}/g"`
 			
