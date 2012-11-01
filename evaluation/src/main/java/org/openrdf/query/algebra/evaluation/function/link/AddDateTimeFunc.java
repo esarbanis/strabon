5ceb4a5b1a4a107e1b84597b10fcbeadb6d236cb
Konstantina Bereta <Konstantina.Bereta@di.uoa.gr>
2012-11-01 19:52:56 +0200

    over and out with addDateTimeFunc. works in select clause but in filter functions comparisons between timezoned and non-timezoned xsd datetimes (exported by this function)  seem to have a problem.

--- evaluation/src/main/java/org/openrdf/query/algebra/evaluation/function/link/AddDateTimeFunc.java
+++ evaluation/src/main/java/org/openrdf/query/algebra/evaluation/function/link/AddDateTimeFunc.java
@@ -2,7 +2,10 @@ package org.openrdf.query.algebra.evaluation.function.link;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.openrdf.model.Value;
@@ -19,7 +22,8 @@ public class AddDateTimeFunc implements Function {
 	
 	@Override
 	public String getURI() {
-		return GeoConstants.stRDF+name;
+		return "http://example.org/custom-function/addDateTime";
+		
 	}
 
     @SuppressWarnings("deprecation")
@@ -29,32 +33,53 @@ public class AddDateTimeFunc implements Function {
             throw new ValueExprEvaluationException("strdf:" + name
                     + " requires exactly 2 arguments, got " + args.length);
         }
-        SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DDThh:mm:ss"); //the format of xsd:Datetime
-		XMLGregorianCalendar calendar=null;
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-DD'T'hh:mm:ss"); //the format of xsd:Datetime
+		GregorianCalendar calendar = new GregorianCalendar();
+		GregorianCalendar cal = new GregorianCalendar(); 
+	    DatatypeFactory dafa = null;
+	    XMLGregorianCalendar xmlcal =null;
+	    XMLGregorianCalendar gxml1 =null;
 
     	try {
-    	    int minutesToAdd = Integer.parseInt(args[1].toString());
+    		String toParse = args[1].toString().replace("^^<http://www.w3.org/2001/XMLSchema#integer>", "").replace("\"", "");
+    		System.out.println("TO PARSE:"+ toParse);
+    		int minutesToAdd = Integer.parseInt(toParse);
     	    String date = args[0].toString();
-    	    Calendar cal = sdf.getCalendar();
+    	    date = args[0].toString().replace("^^<http://www.w3.org/2001/XMLSchema#dateTime>", "").replace("\"", "");
+    	    //cal = sdf.getCalendar();
     		cal.setTime(sdf.parse(date));
     		System.out.println("OLD TIME:"+cal.getTime());
     		cal.add(Calendar.MINUTE, minutesToAdd);
     		System.out.println("NEW TIME:"+cal.getTime());
+    	   
     		
-    		calendar.setYear(cal.getTime().getYear());
-    		calendar.setYear(cal.getTime().getMonth());
-    		calendar.setYear(cal.getTime().getDay());
-    		calendar.setYear(cal.getTime().getHours());
-    		calendar.setYear(cal.getTime().getMinutes());
-    		calendar.setYear(cal.getTime().getSeconds());
+          // xmlcal =  dafa.newXMLGregorianCalendar(cal.toString());
+
+    		System.out.println(cal.get(Calendar.YEAR));
+    		System.out.println(cal.get(Calendar.MONTH));
+    		System.out.println(cal.get(Calendar.DAY_OF_MONTH));
+    		System.out.println(cal.get(Calendar.HOUR));
+    		System.out.println(cal.get(Calendar.MINUTE));
+    		System.out.println(cal.get(Calendar.MILLISECOND));
     		
     	} catch (java.text.ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-        return valueFactory.createLiteral(calendar);
+    
+        //valueFactory.createLiteral(xmlmcal);
+    	XMLGregorianCalendar gxml=null;
+		try {
+			gxml = DatatypeFactory.newInstance().newXMLGregorianCalendar(cal);
+		} catch (DatatypeConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//gxml = dafa.newXMLGregorianCalendar(cal);
+		Value value =  valueFactory.createLiteral(gxml);
+		System.out.println("value="+value.toString());
+		return value;
     }
 
 
