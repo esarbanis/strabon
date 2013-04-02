70d4d05f3eaffae474a87e15730e231df3e944a3
Stella Giannakopoulou <sgian@di.uoa.gr>
2013-04-02 11:35:31 +0300

    #23 forgot to commit a file

---
+++ generaldb/src/main/java/org/openrdf/sail/generaldb/algebra/GeneralDBSqlSpatialMetricTriple.java
@@ -0,0 +1,20 @@
+package org.openrdf.sail.generaldb.algebra;
+
+import org.openrdf.sail.generaldb.algebra.base.GeneralDBQueryModelVisitorBase;
+import org.openrdf.sail.generaldb.algebra.base.GeneralDBSqlExpr;
+import org.openrdf.sail.generaldb.algebra.base.TripleGeneralDBOperator;
+
+public class GeneralDBSqlSpatialMetricTriple extends TripleGeneralDBOperator
+{
+	public GeneralDBSqlSpatialMetricTriple(GeneralDBSqlExpr left, GeneralDBSqlExpr right, GeneralDBSqlExpr third) 
+	{
+		super(left, right, third);
+	}
+
+	@Override
+	public <X extends Exception> void visit(GeneralDBQueryModelVisitorBase<X> visitor) throws X
+	{
+		visitor.meet(this);
+	}
+	
+}
