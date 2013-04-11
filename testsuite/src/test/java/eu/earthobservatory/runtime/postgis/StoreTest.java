e7e3577769ee9185fc4b235a7df60e47e9259f8e
Panayiotis Smeros <psmeros@di.uoa.gr>
2013-04-11 13:48:07 +0300

    Added TemplateTest class. Now test-classes only have to extend this class.

--- testsuite/src/test/java/eu/earthobservatory/runtime/postgis/StoreTest.java
+++ testsuite/src/test/java/eu/earthobservatory/runtime/postgis/StoreTest.java
@@ -9,37 +9,10 @@
  */
 package eu.earthobservatory.runtime.postgis;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
 /**
  * A simple store test. It tests if the input dataset is stored and retrieved correctly.
  * 
  * @author Panayiotis Smeros <psmeros@di.uoa.gr
  */
-public class StoreTest
-{	
-	private final String datasetFile="/"+this.getClass().getSimpleName()+".nt";
-	private final String queryFile="/"+this.getClass().getSimpleName()+".rq";
-	private final String resultsFile="/"+this.getClass().getSimpleName()+".sr";
-
-	@Before
-	public void before() throws Exception
-	{
-		Utils.createdb();
-		Utils.storeDataset(datasetFile);
-	}
-	
-	@Test
-	public void test() throws Exception
-	{
-		Utils.testQuery(queryFile, resultsFile);
-	}
-	
-	@After
-	public void after() throws Exception
-	{
-		Utils.dropdb();
-	}
-}
+public class StoreTest extends TemplateTest{}
---
+++ testsuite/src/test/java/eu/earthobservatory/runtime/postgis/TemplateTest.java
@@ -0,0 +1,64 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * Copyright (C) 2010, 2011, 2012, 2013 Pyravlos Team
+ * 
+ * http://www.strabon.di.uoa.gr/
+ */
+package eu.earthobservatory.runtime.postgis;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * A template test. It: 
+ * 1) creates a database
+ * 2) stores a dataset
+ * 3) poses a query
+ * 4) checks if the results of the query are the expected 
+ * 5) drops the database
+ * 
+ * @author Panayiotis Smeros <psmeros@di.uoa.gr
+ */
+public abstract class TemplateTest
+{	
+	private String datasetFile;
+	private String queryFile;
+	private String resultsFile;
+
+	public TemplateTest(String datasetFile, String queryFile, String resultsFile)
+	{
+		this.datasetFile = datasetFile;
+		this.queryFile = queryFile;
+		this.resultsFile = resultsFile;
+	}
+
+	public TemplateTest()
+	{
+		this.datasetFile="/"+this.getClass().getSimpleName()+".nt";
+		this.queryFile="/"+this.getClass().getSimpleName()+".rq";
+		this.resultsFile="/"+this.getClass().getSimpleName()+".sr";
+	}
+
+	@Before
+	public void before() throws Exception
+	{
+		Utils.createdb();
+		Utils.storeDataset(datasetFile);
+	}
+	
+	@Test
+	public void test() throws Exception
+	{
+		Utils.testQuery(queryFile, resultsFile);
+	}
+	
+	@After
+	public void after() throws Exception
+	{
+		Utils.dropdb();
+	}
+}
